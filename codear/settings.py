2d9e4ae16b212a7605feb16a67322598086314d5
Agustin Carrasco <asermax@gmail.com>
2020-08-14 00:33:36 -0300

    fix(django): move secret key to an env variable

--- codear/settings.py
+++ codear/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l^ogjn$zwo@jd0wijpxr2r217vq%zbc!n@&(3q0r1h7-7e)x50'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('LAMBDA_TASK_ROOT') is None
