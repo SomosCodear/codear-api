01f3b1ebca3e6e8a60b386a1355ed599cce25383
Agustin Carrasco <asermax@gmail.com>
2020-08-17 23:09:58 -0300

    feat(events): add cache control for API

--- events/views.py
+++ events/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
 from . import models, serializers, filters
 
@@ -6,3 +8,7 @@ class EventViewset(viewsets.ReadOnlyModelViewSet):
     queryset = models.Event.objects.all()
     serializer_class = serializers.EventSerializer
     filterset_class = filters.EventFilterSet
+
+    @method_decorator(cache_page(60 * 60 * 24))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
